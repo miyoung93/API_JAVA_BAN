3c4cebc3301961e5fdf791186da9a9d9410c2b95
psuu0007 <psuu0007@naver.com>
2023-06-15 16:55:15 +0900

    구구단 학습

---
+++ Gugudan/src/ezen/Gugudan.java
@@ -0,0 +1,15 @@
+package ezen;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("구구단 연습문제");
+		
+		System.out.println(2 * 1);
+		System.out.println(2 * 9);
+		
+	}
+
+}
